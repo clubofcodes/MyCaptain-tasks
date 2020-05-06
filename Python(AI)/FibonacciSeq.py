15117aeb0c5cde1a39ef320f7d784f7299efa0e3
Rahul Jagetiya <63360644+clubofcodes@users.noreply.github.com>
2020-05-06 22:16:43 +0530

    Update FibonacciSeq.py

--- Python(AI)/FibonacciSeq.py
+++ Python(AI)/FibonacciSeq.py
@@ -1,1 +1,19 @@
+# Python program to display the Fibonacci sequence
 
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+#As per the user input
+nterms = int(input("Enter the no. of terms:"))
+#print("\n")
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("\nFibonacci sequence till",nterms,"terms is:")
+   for i in range(nterms):
+       print(recur_fibo(i))
